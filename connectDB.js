19c99f12ad32c85f3610e691f216fc5033e85d82
wasiq abdullah <chwasiq0569>
2023-01-29 11:52:47 +0500

    added CONSSTR

--- connectDB.js
+++ connectDB.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
+        const conn = await mongoose.connect("mongodb+srv://admin:admin@cluster0.bekcudd.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
