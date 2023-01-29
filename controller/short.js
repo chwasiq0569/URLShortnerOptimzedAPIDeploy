6d1941f3ec48b53e51ecab2c452d2ce221835145
wasiq abdullah <chwasiq0569>
2023-01-29 11:50:03 +0500

    Updated Backend

--- controller/short.js
+++ controller/short.js
@@ -1,25 +1,42 @@
 import shortUrl from '../model/shortStore.js';
 import { nanoid } from 'nanoid';
 import validUrl from 'valid-url';
+import moment from 'moment';
+import URI from "uri-js";
+
+const fixURL = (url) => {
+    if (!URI.parse(url).scheme) {
+        return 'http://' + url;
+    } else return url;
+}
 
 export const createShortURL = async (req, res) => {
-    console.log('APICALL', req.body.full)
+    console.log('APICALL', !validUrl.isUri(req.body.full))
     console.log('NANOID', nanoid());
+    console.log('URI', fixURL(req.body.full));
 
-    var query = { full: req.body.full }
-    var update = { short: nanoid() }
-    var options = { upsert: true, new: true, setDefaultsOnInsert: true };
-
-    if (!validUrl.isUri(req.body.full)) {
+    if (!validUrl.isUri(fixURL(req.body.full))) {
         return res.status(401).json({
             message: 'Unable to shorten that link. It is not a valid url!'
         })
     }
-
     try {
-        const shortURL = await shortUrl.findOneAndUpdate(query, update, options);
-        console.log('shortURL', shortURL)
-        res.send(shortURL);
+        const found = await shortUrl.find({ full: req.body.full }).sort({ createdAt: -1 }).limit(1);
+        if (found.length > 0) {
+            let hours = moment().diff(moment(found[0].createdAt), 'hours');
+            console.log("hours", hours)
+            if (hours >= 5) {
+                const shortURL = await shortUrl.create({ full: req.body.full, short: nanoid() });
+                console.log('shortURL', shortURL)
+                res.send(shortURL);
+            } else {
+                res.send(found[0]);
+            }
+        } else {
+            const shortURL = await shortUrl.create({ full: req.body.full, short: nanoid() });
+            console.log('shortURL', shortURL)
+            res.send(shortURL);
+        }
     }
     catch (err) {
         res.status(501).json({
--- model/shortStore.js
+++ model/shortStore.js
@@ -10,7 +10,7 @@ const shortSchema = new mongoose.Schema({
     type: String,
     required: true,
   }
-});
+}, { timestamps: true });
 
 shortSchema.index({ full: 1 });
 
